d55c80fb41309c976b53d2256151896f8058d058
majdh98 <66201039+majdh98@users.noreply.github.com>
2023-01-09 20:26:39 -0500

    Update blinky_Step4.c
    Added libraries and fixed naming

--- Module 1/blinky_Step4.c
+++ Module 1/blinky_Step4.c
@@ -14,9 +14,12 @@
 #include "platform.h"						/* ZYBOboard interface */
 #include <xgpio.h>							/* Xilinx GPIO functions */
 #include "xparameters.h"				/* constants used by the hardware */
+#include <stdbool.h> 				// bool type
+#include <stdlib.h> //strtol
 
 #define OUTPUT 0x0							/* setting GPIO direction to output */
 #define CHANNEL1 1							/* channel 1 of the GPIO port */
+
 
 #define LINELEN 32
 
@@ -43,61 +46,61 @@ void read_save_echo_line(char line[]){
     *(line+i) = '\0';
 }
 
-
 int main() {
-   XGpio port;									/* GPIO port connected to the leds */
-   char line[LINELEN];
-   bool done = false;
-   char* end_char; // first non-integer char of a line (used in strtol())
-   int strtol_n; // strtol return
+
+	XGpio port;									/* GPIO port connected to the leds */
+	char line[LINELEN];
+	bool done = false;
+	char* end_char; // first non-integer char of a line (used in strtol())
+	int strtol_n; // strtol return
 
 
 
-   init_platform();							/* initialize the hardware platform */
+	init_platform();							/* initialize the hardware platform */
 
-   /*
-     * set stdin unbuffered, forcing getchar to return immediately when
-     * a character is typed .
-     */
-   setvbuf(stdin,NULL,_IONBF,0);
+	/*
+	 * set stdin unbuffered, forcing getchar to return immediately when
+	 * a character is typed .
+	 */
+	setvbuf(stdin,NULL,_IONBF,0);
 
-   printf("[Hello]\n");
+	printf("[Hello]\n");
 
-   XGpio_Initialize(&port, XPAR_AXI_GPIO_0_DEVICE_ID);	/* initialize device AXI_GPIO_0 */
-   XGpio_SetDataDirection(&port, CHANNEL1, OUTPUT);	    /* set tristate buffer to output */
-   XGpio_DiscreteWrite(&port, CHANNEL1, 0x1);						/* turn on led0 */
+	XGpio_Initialize(&port, XPAR_AXI_GPIO_0_DEVICE_ID);	/* initialize device AXI_GPIO_0 */
+	XGpio_SetDataDirection(&port, CHANNEL1, OUTPUT);	    /* set tristate buffer to output */
+	XGpio_DiscreteWrite(&port, CHANNEL1, 0x1);						/* turn on led0 */
 
 
-   while(!done){
+	while(!done){
 
-       printf(">");
-       // write everything in stdout buffer in case
-       fflush(stdout);
+		printf(">");
+		// write everything in stdout buffer in case
+		fflush(stdout);
 
-       read_save_echo_line(line);
+		read_save_echo_line(line);
 
-       if (strcmp(line, "q") == 0){
-           done = true;
-       }else{
-           strtol_n = (int)strtol(line, &end_char, 10); // convert the content of line to base 10
-           /*
-           detect a number between 0 and 3
-               man strtol:
-               If endptr is not NULL, strtol() stores the address of the first invalid
-               character  in *endptr.  If there were no digits at all, strtol() stores
-               the original value of nptr in *endptr (and returns 0).  In  particular,
-               if  *nptr is not '\0' but **endptr is '\0' on return, the entire string
-               is valid.
-           */
-           if (*line != '\0' && *end_char == '\0'){ //or *end_char == '\0' && line != end_char && n >=0){
-               if (n >= 0 && n <= 3){
-                   printf("\n[%d]",n);
-               }
-           }
-       }
-       printf("\n");
-   }
+		if (strcmp(line, "q") == 0){
+			done = true;
+		}else{
+			strtol_n = (int)strtol(line, &end_char, 10); // convert the content of line to base 10
+			/*
+	           detect a number between 0 and 3
+	               man strtol:
+	               If endptr is not NULL, strtol() stores the address of the first invalid
+	               character  in *endptr.  If there were no digits at all, strtol() stores
+	               the original value of nptr in *endptr (and returns 0).  In  particular,
+	               if  *nptr is not '\0' but **endptr is '\0' on return, the entire string
+	               is valid.
+			 */
+			if (*line != '\0' && *end_char == '\0'){ //or *end_char == '\0' && line != end_char && n >=0){
+				if (strtol_n >= 0 && strtol_n <= 3){
+					printf("\n[%d]",strtol_n);
+				}
+			}
+		}
+		printf("\n");
+	}
 
-   cleanup_platform();					/* cleanup the hardware platform */
-   return 0;
+	cleanup_platform();					/* cleanup the hardware platform */
+	return 0;
 }
