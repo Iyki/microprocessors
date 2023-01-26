07545083fb60510ef16d5d02b26e20d3ad67088c
majdh98 <66201039+majdh98@users.noreply.github.com>
2023-01-26 16:22:04 -0500

    Create servo.c

---
+++ Module3/step6./servo.c
@@ -0,0 +1,17 @@
+/*
+ * servo.c
+ */
+
+/*
+ * Initialize the servo, setting the duty cycle to 7.5%
+ */
+void servo_init(void) {
+	return;
+}
+
+/*
+ * Set the dutycycle of the servo
+ */
+void servo_set(double dutycycle) {
+	return;
+}
