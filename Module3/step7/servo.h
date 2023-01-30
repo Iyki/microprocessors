f1cbaeab03ede1a4b97d374fadffacc7f87f4fdb
Mubarak Idoko <66747577+mubbie@users.noreply.github.com>
2023-01-30 14:16:54 -0500

    Update servo.h

--- Module3/step7/servo.h
+++ Module3/step7/servo.h
@@ -8,6 +8,13 @@
 #include "xparameters.h"  	/* constants used by the hardware */
 #include "xil_types.h"		/* types used by xilinx */
 
+// hardware 
+#define CLOCK_FREQ 50000000
+
+// timer 0 settings
+#define PERIOD 1000000 // 20ms period (Timer 0)
+#define PERIOD_MS 20 // 20ms period (Timer 0)
+
 /*
  * Initialize the servo, setting the duty cycle to 7.5%
  */
