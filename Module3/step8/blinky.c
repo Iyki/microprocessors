ae7798ed94f05008bd8f704261456ba6e3cf1a63
majdh98 <66201039+majdh98@users.noreply.github.com>
2023-01-31 03:57:34 -0500

    completed step 10

--- Module3/step8/blinky.c
+++ Module3/step8/blinky.c
@@ -12,15 +12,13 @@
 #include "platform.h"		/* ZYBO board interface */
 #include "xil_types.h"		/* u32, s32 etc */
 #include "xparameters.h"	/* constants used by hardware */
-#include "xtmrctr.h"
-
 
 #include "gic.h"		/* interrupt controller interface */
 #include "xgpio.h"		/* axi gpio interface */
 #include "led.h" // led interface
 #include "io.h" //
 #include "ttc.h"
-#include "servo.h"
+
 
 
 
@@ -34,16 +32,20 @@
 #define FREQ 1 // 1 hz = 1 sec
 #define LED4 4
 
+static bool led4_on = false;
+
+
 #define PERIOD 1000000
 #define HIGHTIME 75000
 #define INITIAL_SERVO_DUTYCYCLE 7.5
+
 
 void read_save_echo_line(char line[]){
     char c;
     int i = 0; // where to save read char in line
     c = getchar();
 
-    // \r is carriage return which is what lines end with in this system
+    // \r is carriage return which is what lines end with in tahis system
     // https://developer.arm.com/documentation/ka003309/latest#:~:text=In%20most%20C%20compilers%2C%20including,return%20is%20'%5Cr'.
     while (c != '\r'){
         // save read char
@@ -63,11 +65,12 @@ void read_save_echo_line(char line[]){
 
 // toggle led 4
 static void ttc_callback(void){
-	if (led_get(LED4) == LED_ON){
-		led_set(4, LED_OFF);
+	if (led4_on){
+		led_set(LED4, LED_OFF);
 	}else{
-		led_set(4, LED_ON);
+		led_set(LED4, LED_ON);
 	}
+	led4_on = !led4_on;
 }
 
 void btn_callback(u32 btn) {
@@ -84,6 +87,7 @@ int main() {
 	bool done = false;
 	char* end_char; // first non-integer char of a line (used in strtol())
 	int strtol_n; // strtol return
+
     double servo_dutycycle = INITIAL_SERVO_DUTYCYCLE; // 7.5% dutycycle
 
 	// initialize platform
@@ -99,8 +103,8 @@ int main() {
 	// initialize and start ttc
 	ttc_init(FREQ, ttc_callback);
 	ttc_start();
-	// initialize the servo
-    	servo_init(); // starts at initial servo dutycycle
+    // initialize the servo
+    servo_init(); // starts at initial servo dutycycle
 
 	//set led 4 on to signify program is on
 	led_set(4, LED_ON);
@@ -111,40 +115,50 @@ int main() {
 	// listening for stdin
 	while(!done){
 
-		printf(">");
-		// write everything in stdout buffer in case
-		fflush(stdout);
+			printf(">");
+			// write everything in stdout buffer in case
+			fflush(stdout);
 
-		read_save_echo_line(line);
+			read_save_echo_line(line);
 
-		if (strcmp(line, "q") == 0){
-			done = true;
-            	}else if (strcmp(line, "a") == 0){
-                	// increment duty cycle
-                	servo_dutycycle += 0.25;
-                	servo_set(servo_dutycycle);
-                	printf("[%f]", servo_dutycycle);
-            	}else if (strcmp(line, "s") == 0){
-                	// decrement duty cycle
-                	servo_dutycycle -= 0.25;
-                	servo_set(servo_dutycycle);
-               		printf("[%f]", servo_dutycycle);
-		}else{
-			strtol_n = (int)strtol(line, &end_char, 10); // convert the content of line to base 10
-			/*
-		        detect a number between 0 and 3
-		        man strtol:
-		        If endptr is not NULL, strtol() stores the address of the first invalid
-		        character  in *endptr.  If there were no digits at all, strtol() stores
-		       	the original value of nptr in *endptr (and returns 0).  In  particular,
-		        if  *nptr is not '\0' but **endptr is '\0' on return, the entire string
-		        is valid.
-			*/
-			if (*line != '\0' && *end_char == '\0'){ //or *end_char == '\0' && line != end_char && n >=0){
-				if (strtol_n >= 0 && strtol_n <= 3){
-					// Step 9: Toggle the expected LED & print
-					led_toggle((u32)strtol_n);
-				}
+			if (strcmp(line, "q") == 0){
+				done = true;
+			}else if (strcmp(line, "a") == 0){
+                // increment duty cycle
+                servo_dutycycle += 0.25;
+                servo_set(servo_dutycycle);
+                printf("[%f]", servo_dutycycle);
+            }else if (strcmp(line, "s") == 0){
+                // decrement duty cycle
+                servo_dutycycle -= 0.25;
+                servo_set(servo_dutycycle);
+                printf("[%f]", servo_dutycycle);
+            }else if (strcmp(line, "low") == 0){
+                // decrement duty cycle
+                servo_dutycycle = MINPOINT;
+                servo_set(servo_dutycycle);
+                printf("[%f]", servo_dutycycle);
+            }else if (strcmp(line, "high") == 0){
+                // decrement duty cycle
+                servo_dutycycle = MAXPOINT;
+                servo_set(servo_dutycycle);
+                printf("[%f]", servo_dutycycle);
+            }else{
+				strtol_n = (int)strtol(line, &end_char, 10); // convert the content of line to base 10
+				/*
+		           detect a number between 0 and 3
+		               man strtol:
+		               If endptr is not NULL, strtol() stores the address of the first invalid
+		               character  in *endptr.  If there were no digits at all, strtol() stores
+		               the original value of nptr in *endptr (and returns 0).  In  particular,
+		               if  *nptr is not '\0' but **endptr is '\0' on return, the entire string
+		               is valid.
+				 */
+				if (*line != '\0' && *end_char == '\0'){ //or *end_char == '\0' && line != end_char && n >=0){
+					if (strtol_n >= 0 && strtol_n <= 3){
+						// Step 9: Toggle the expected LED & print
+						led_toggle((u32)strtol_n);
+					}
 				}
 			}
 			printf("\n");
